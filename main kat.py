b5c53bcaeb032dff9d7c93f2d199eb610b68dbc6
laben <laur104b@zbc.dk>
2023-01-13 15:03:04 +0100

    main kat

---
+++ main kat.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+
+class AdoptCatApp(App):
+   def build(self):
+       layout = GridLayout(cols=3)
+       layout.add_widget(Image(source='cat1.jpg'))
+       layout.add_widget(Label(text='[b]Name:[/b] Whiskers\n[b]Age:[/b] 2 years\n[b]Breed:[/b] Siamese'))
+       layout.add_widget(Button(text='Adopt', on_press=self.adopt_cat))
+
+       layout.add_widget(Image(source='cat2.jpg'))
+       layout.add_widget(Label(text='[b]Name:[/b] Mittens\n[b]Age:[/b] 1 year\n[b]Breed:[/b] Maine Coon'))
+       layout.add_widget(Button(text='Adopt', on_press=self.adopt_cat))
+
+       layout.add_widget(Image(source='cat3.jpg'))
+       layout.add_widget(Label(text='[b]Name:[/b] snuske\n[b]Age:[/b]  year\n[b]Breed:[/b] Calico'))
+       layout.add_widget(Button(text='Adopt', on_press=self.adopt_cat))
+
+       return layout
+
+   def adopt_cat(self, instance):
+       print("Cat adopted!")
+
+if __name__ == '__main__':
+   AdoptCatApp().run()
