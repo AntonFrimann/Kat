6188d783f89a2a48f6fc1b4db3a070380223ee72
laben <laur104b@zbc.dk>
2023-01-18 11:15:55 +0100

    main kat og import af API

--- API.py
+++ API.py
@@ -1,9 +1,22 @@
 import requests
 
-response = requests.get('https://api.thecatapi.com/v1/images/search?limit=10')
+def getData():
+    response = requests.get('https://api.thecatapi.com/v1/images/search?limit=10')
 
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print("Error: API request failed")
+    if response.status_code == 200:
+        data = response.json()
+        return data
+
+    else:
+        print("Error: API request failed")
+        return []
+
+def getUrls():
+    data = getData()
+    urls = []
+    for cat in data:
+        urls.append(cat['url'])
+    return urls
+
+if __name__ == "__main__":
+    print(getData())
--- main kat.py
+++ main kat.py
@@ -4,25 +4,28 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 
+from API import getUrls
+
 class AdoptCatApp(App):
-   def build(self):
-       layout = GridLayout(cols=3)
-       layout.add_widget(Image(source='cat1.jpg'))
-       layout.add_widget(Label(text='[b]Name:[/b] Whiskers\n[b]Age:[/b] 2 years\n[b]Breed:[/b] Siamese'))
-       layout.add_widget(Button(text='Adopt', on_press=self.adopt_cat))
 
-       layout.add_widget(Image(source='cat2.jpg'))
-       layout.add_widget(Label(text='[b]Name:[/b] Mittens\n[b]Age:[/b] 1 year\n[b]Breed:[/b] Maine Coon'))
-       layout.add_widget(Button(text='Adopt', on_press=self.adopt_cat))
+    def build(self):
+        layout = GridLayout(cols=3)
+        layout.add_widget(Image(source='cat1.jpg'))
+        layout.add_widget(Label(text='[b]Name:[/b] Whiskers\n[b]Age:[/b] 2 years\n[b]Breed:[/b] Siamese'))
+        layout.add_widget(Button(text='Adopt', on_press=self.adopt_cat))
 
-       layout.add_widget(Image(source='cat3.jpg'))
-       layout.add_widget(Label(text='[b]Name:[/b] snuske\n[b]Age:[/b]  year\n[b]Breed:[/b] Calico'))
-       layout.add_widget(Button(text='Adopt', on_press=self.adopt_cat))
+        layout.add_widget(Image(source='cat2.jpg'))
+        layout.add_widget(Label(text='[b]Name:[/b] Mittens\n[b]Age:[/b] 1 year\n[b]Breed:[/b] Maine Coon'))
+        layout.add_widget(Button(text='Adopt', on_press=self.adopt_cat))
 
-       return layout
+        layout.add_widget(Image(source='cat3.jpg'))
+        layout.add_widget(Label(text='[b]Name:[/b] snuske\n[b]Age:[/b]  year\n[b]Breed:[/b] Calico'))
+        layout.add_widget(Button(text='Adopt', on_press=self.adopt_cat))
 
-   def adopt_cat(self, instance):
-       print("Cat adopted!")
+        return layout
+
+    def adopt_cat(self, instance):
+        print("Cat adopted!")
 
 if __name__ == '__main__':
-   AdoptCatApp().run()
+    AdoptCatApp().run()
